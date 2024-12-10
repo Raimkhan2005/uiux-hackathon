1c59368ca3e7e6365509401746de9d8a4956aa11
Raimkhan2005 <160727210+Raimkhan2005@users.noreply.github.com>
2024-12-11 03:39:15 +0500

    code update

--- src/app/Components/TopHeader.tsx
+++ src/app/Components/TopHeader.tsx
@@ -7,13 +7,13 @@ export default function TopHeader() {
         <div className='flex justify-between bg-[#272343] w-auto h-[45px]' >
 
         <div className='flex items-center'>
-          <p className='text-white flex items-center pl-[300px] font-normal  text-[13px] leading-[14.3px]'> <Check classname="w-[10.67px] h-[7.33px]"/>Free shipping on all orders over $50</p>
+          <p className='text-white flex items-center pl-[300px] font-normal  text-[13px] leading-[14.3px]'> <Check className="w-[16px] h-[16px]"/>Free shipping on all orders over $50</p>
         </div>
 
         <div className='flex pr-[300px] space-x-5'>
-            <button className='text-white flex items-center  gap-1 font-normal text-[13px] leading-[16.9px] '>Eng <ChevronDown classname="w-7 h-[3.5px]" /></button>
+            <button className='text-white flex items-center  gap-1 font-normal text-[13px] leading-[16.9px] '>Eng <ChevronDown size={20} /></button>
             <button className='text-white flex items-center font-normal text-[13px] leading-[16.9px]'>Fags</button>
-            <button className='text-white flex items-center gap-2 font-normal text-[13px] leading-[16.9px]'><Info classname="w-16 h-16"/>Need help</button>
+            <button className='text-white flex items-center gap-2 font-normal text-[13px] leading-[16.9px]'><Info className="w-[16px] h-[16px]"/>Need help</button>
         </div>
 
         </div>
